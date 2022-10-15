b3d79c4a6eaace66912bb197c8a4b219176da34e
Dave Godfrey <davegod@bu.edu>
2022-10-15 12:17:52 -0400

    describe

--- deliverable1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def read_csv(filepath):
-    df = pd.read_csv(filepath)
-    return df
-
-df = read_csv("/Users/davegodfrey/Desktop/CS Degree/Computer Science/CS 506/homework-1-DaveG77/train.csv")
-df.describe()
